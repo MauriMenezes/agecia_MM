295d61e745d16ff70dc7a948578531c3ff5ced3f
Maurilio Menezes <mauriliosm25@gmail.com>
2022-10-23 14:03:09 -0300

    feat: footer e btn style

--- src/pages/registerPage/index.jsx
+++ src/pages/registerPage/index.jsx
@@ -2,6 +2,8 @@ import "./registerStyle.css"
 import { json, useNavigate } from "react-router-dom";
 import React, { useState } from "react"
 import { Link } from "react-router-dom";
+import Footer from "../../components/Footer";
+import { Button } from "react-bootstrap";
 const Register = () => {
   const navigate = useNavigate()
 
@@ -46,61 +48,63 @@ const Register = () => {
 
   return (
 
-    <div id="reg">
-      <h2 className="">CADASTRO  DO SISTEMA</h2>
-      <form className="form" onSubmit={submit}>
+    <>
+      <div id="reg">
+        <form className="form" onSubmit={submit}>
+          <h2 className="titulo-login">Cadastrar</h2>
 
-        <div className="field">
-          <label>Email</label>
-          <input type={"email"}
-            name="email"
-            id="email"
-            placeholder="Email@email.com"
-            onChange={(e) => setEmail(e.target.value)}
-          ></input>
-        </div>
-        <div className="field">
-          <label>Nome</label>
-          <input type={"text"}
-            name="name"
-            id="name"
-            placeholder="Seu Nome aqui"
+          <div className="field">
+            <label>Email</label>
+            <input type={"email"}
+              name="email"
+              id="email"
+              placeholder="Email@email.com"
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+          </div>
+          <div className="field">
+            <label>Nome</label>
+            <input type={"text"}
+              name="name"
+              id="name"
+              placeholder="Seu Nome aqui"
 
-            onChange={(e) => setNome(e.target.value)}
-          ></input>
-        </div>
+              onChange={(e) => setNome(e.target.value)}
+            ></input>
+          </div>
 
-        <div className="field">
-          <label>CPF</label>
-          <input type={"number"}
-            name="cpf"
-            id="cpf"
-            placeholder="XXX.XXX.XXX-XX"
+          <div className="field">
+            <label>CPF</label>
+            <input type={"number"}
+              name="cpf"
+              id="cpf"
+              placeholder="XXX.XXX.XXX-XX"
 
-            onChange={(e) => setCpf(e.target.value)}
-          ></input>
-        </div>
-        <div className="field">
-          <label>Senha</label>
-          <input type={"password"}
-            name="password"
-            id="password"
-            placeholder="Sua senha aqui"
+              onChange={(e) => setCpf(e.target.value)}
+            ></input>
+          </div>
+          <div className="field">
+            <label>Senha</label>
+            <input type={"password"}
+              name="password"
+              id="password"
+              placeholder="*********"
 
-            onChange={(e) => setSenha(e.target.value)}
-          ></input>
-        </div>
-        <div className="actions">
-          <button type="submit">CADASTRAR</button>
-        </div>
-        <div className="reg">
-          <h4>Já possui conta? Entre  <Link className="link" to={"/login"} >
-            aqui
-          </Link>  </h4>
-        </div>
-      </form>
-    </div>
-
+              onChange={(e) => setSenha(e.target.value)}
+            ></input>
+          </div>
+          <div className="actions">
+            <Button type="submit" className="btn-register">CADASTRAR</Button>
+          </div>
+          <div className="reg">
+            <h4>Já possui conta? Entre  <Link className="link" to={"/login"} >
+              aqui
+            </Link>  </h4>
+          </div>
+        </form>
+      </div>
+      <Footer />
+    </>
   )
 
 }
