cecf3124d7cc0d4960201f23201c1070a36636f5
Maurilio Menezes <mauriliosm25@gmail.com>
2022-10-23 14:04:28 -0300

    feat: componente footer

--- src/pages/loginPage/index.jsx
+++ src/pages/loginPage/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useContext } from "react"
 import { AuthContext } from "../../context/auth"
 import { Link } from "react-router-dom"
-import { Button, Alert } from "react-bootstrap"
+import { Button, Alert, Container } from "react-bootstrap"
 
 import "./login.css"
+import Footer from "../../components/Footer"
 
 
 const Login = () => {
@@ -23,46 +24,54 @@ const Login = () => {
   }
   return (
 
-    <div id="login">
-      <h1 className="titulo">LOGIN DO SISTEMA</h1>
-      <p>{String(authenticated)}</p>
-      <form className="form" onSubmit={handleSubmit}>
+    <>
 
-        <div className="field">
-          <label>Email</label>
+      <div id="login">
 
-          <input type={"email"}
-            name="password"
-            id="password"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)} required></input>
-        </div>
-        <div className="field">
-          <label>Senha</label>
-          <input type={"password"}
-            name="password"
-            id="password"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}></input>
-        </div>
-        <div className="actions">
-          <div className="d-grid gap-2">
-            <Button type='submit' className="btn-login" variant="primary" size="lg">
-              Entrar
-            </Button>
+        <h1 className="titulo-login">Login </h1>
+        <form className="form" onSubmit={handleSubmit}>
+
+          <div className="field">
+            <label>Email</label>
+
+            <input type={"email"}
+              name="password"
+              id="password"
+              value={email}
+              placeholder="Email@email.com"
+              onChange={(e) => setEmail(e.target.value)} required></input>
+          </div>
+          <div className="field">
+            <label>Senha</label>
+            <input type={"password"}
+              name="password"
+              id="password"
+              value={senha}
+              placeholder="**********"
+              onChange={(e) => setSenha(e.target.value)}></input>
+          </div>
+          <div className="actions">
+            <div className="d-grid gap-2">
+              <Button type='submit' className="btn-login" variant="primary" size="lg">
+                Entrar
+              </Button>
+
+            </div>
+
 
           </div>
+          <div className="reg">
+            <h4>Nâo possui conta? Registre-se <Link className="link" to={"/register"} >
+              aqui
+            </Link>  </h4>
+          </div>
+        </form>
 
+      </div>
 
-        </div>
-        <div className="reg">
-          <h4>Nâo possui conta? Registre-se <Link className="link" to={"/register"} >
-            aqui
-          </Link>  </h4>
-        </div>
-      </form>
+      <Footer />
+    </>
 
-    </div>
 
 
   )
