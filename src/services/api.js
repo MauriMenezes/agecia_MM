4622bef6909e673986377231f28cc4bc1f498bb1
Maurilio Menezes <mauriliosm25@gmail.com>
2022-10-22 22:19:08 -0300

    feat: endereço da api no heroku

--- src/services/api.js
+++ src/services/api.js
@@ -2,9 +2,10 @@ import axios from "axios"
 
 export const api = axios.create({
 
-  baseURL: "http://localhost:8080",
+  baseURL: "https://agencia-viagem-recode.herokuapp.com",
 })
 export const createAuth = async (email, senha) => {
 
   return api.post("/auth", { email, senha })
 }
+
